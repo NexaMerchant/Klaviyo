a6f597f6943380aab727c46e8f391cf3977f66ed
xxl4 <nice.lizhi@gmail.com>
2025-02-27 09:51:14 +0000

    get all profile to add special lists

--- src/Console/Commands/Profile/Lists.php
+++ src/Console/Commands/Profile/Lists.php
@@ -3,12 +3,21 @@ namespace NexaMerchant\Klaviyo\Console\Commands\Profile;
 
 use NexaMerchant\Apps\Console\Commands\CommandInterface;
 use KlaviyoAPI\KlaviyoAPI;
+use Illuminate\Support\Facades\Redis;
 
 class Lists extends CommandInterface 
 {
     protected $signature = 'Klaviyo:profile:lists';
 
     protected $description = 'Get Klaviyo profile lists';
+
+    private $klaviyo;
+
+    private $page_cursor = null;
+
+    private $client;
+
+    private $list_id = "Rua5Sy";
 
     public function getAppVer() {
         return config("Klaviyo.ver");
@@ -22,15 +31,78 @@ class Lists extends CommandInterface
     {
         $this->info("Get Klaviyo profile lists");
 
-        $klaviyo = new KlaviyoAPI(config("Klaviyo.api_key"));
+        $this->klaviyo = new KlaviyoAPI(config("Klaviyo.api_key"));
+
+        $list_id = "Rua5Sy";
 
         // Get all profiles Lists
-        $lists = $klaviyo->Profiles->getProfiles(
-            page_size: 1
-        );
-        var_dump($lists);
+        $profiles = $this->getProfiles();   
 
         
+
+        
+    }
+
+    public function getProfiles($next = null) {
+        $profiles = $this->klaviyo->Profiles->getProfiles(
+            page_size: 100,
+            page_cursor: $next
+        );
+
+        $this->client = new \GuzzleHttp\Client();
+        
+        // check the profile in the list, if not exist, create it
+        foreach ($profiles['data'] as $profile) {
+            //var_dump($profile);
+
+            $profile_id = $profile['id'];
+
+            // relationships/profiles
+            $this->info('Pushing profile to Klaviyo...'.$profile_id);
+            //var_dump($profile_id);
+
+            // check if the profile exists
+            $profile_exists = Redis::get('klaviyo_profile_'.$profile_id);
+            if ($profile_exists) {
+                continue;
+            }
+
+            $response = $this->client->request('POST', 'https://a.klaviyo.com/api/lists/'.$this->list_id.'/relationships/profiles', [
+                'body' => '{"data":[{"type":"profile","id":"'.$profile_id.'"}]}',
+                'headers' => [
+                  'Authorization' => 'Klaviyo-API-Key ' . config('mail.mailers.klaviyo.api_key'),
+                  'accept' => 'application/vnd.api+json',
+                  'content-type' => 'application/vnd.api+json',
+                  'revision' => '2025-01-15',
+                ],
+              ]);
+              
+              echo $response->getBody();
+
+              // redis save the profile id
+              Redis::set('klaviyo_profile_'.$profile_id, $profile_id);
+              sleep(1);
+
+
+              //exit;
+
+
+        }
+
+        //var_dump($profiles);
+
+        //exit;
+        
+
+        if ($profiles['links']['next']) {
+            $this->error('Next page...'.$profiles['links']['next']);
+            $this->getProfiles($profiles['links']['next']);
+        }
+
+        //var_dump($profiles);
+        
+
+        return $profiles;        
     }
 
 }
